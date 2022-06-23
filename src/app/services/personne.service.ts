c7f2f91099c4c74d49aafbd6e8a9e86a7e112ff4
n-rick <e.ficadiere@gmail.com>
2022-06-23 09:31:09 +0200

    chore: add interceptor

--- src/app/services/personne.service.ts
+++ src/app/services/personne.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Personne } from '../interfaces/personne';
 
@@ -8,29 +8,25 @@ import { Personne } from '../interfaces/personne';
 export class PersonneService {
 
   private url: string = "https://127.0.0.1:8000/ws/personnes";
+  private headers: HttpHeaders;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders().set("Authorization", `Bearer ${localStorage.getItem('jeton')}`);
+  }
 
-  getAllPersonnes()
-  {
+  getAllPersonnes() {
     return this.http.get<Array<Personne>>(this.url);
   }
-
-  getOnePersonne(id: any)
-  {
+  getOnePersonne(id: any) {
     return this.http.get<Personne>(`${this.url}/${id}`);
   }
-
   addPersonne(p: Personne) {
     return this.http.post<Personne>(this.url, p);
   }
-
   editPersonne(p: Personne) {
     return this.http.put<Personne>(`${this.url}/${p.id}`, p);
   }
-
-
-  removePersonne(id: any){
-    return this.http.delete<Personne>(`${this.url}/${id}`);
+  removePersonne(id: any) {
+    return this.http.delete(`${this.url}/${id}`);
   }
 }
